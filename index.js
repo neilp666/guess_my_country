87960407c5947c265adc67580a178371815b0f27
neil patel <neil@myindianweddingapp.com>
2018-09-02 13:51:02 +0100

    Created a start function to start the game

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ function initialize(event, callback){
       event.session.attributes.Game = {};
   }
 }
+
+var handler = {
+  "start": function() {
+    this.attributes.Game.score = 0;
+    this.attributes.Game.index = 0;
+    this.attributes.Game.count = 3;
+    var question = arr[0].Q;
+    var q = "<break time='0.5s'/>" + question.split('').join("<break time='0.5s'/>") + "<break time='0.5s'/>";
+    var qc = question.split('').join("  ");
+    var speech = "Welcome to Guess My Country, let's begin, here are your jumbled letters " + q + ". Guess the word, you have 3 attempts left ";
+    this.emit(":askWithCard", speech, speech, "Guess My Country", qc);
+  },
+}
